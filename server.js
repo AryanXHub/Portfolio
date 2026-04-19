597d5a9690f3ab38cf3c09c425658ca2d8cb1e5a
AryanXHub <aryangaur369@gmail.com>
2026-04-19 23:21:34 +0530

    Added env file and updated code

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+console.log("ENV VALUE:", process.env.MONGO_URI);
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -6,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+console.log("Mongo URI:", process.env.MONGO_URI);
 
 mongoose
   .connect(process.env.MONGO_URI)
